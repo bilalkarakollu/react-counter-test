35f6a49f01a77d25321b0386bbd8a113010a4bae
bilalkarakollu <karakollubilal@gmail.com>
2022-04-28 23:08:10 +0300

    counter complated

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import styles from "./Counter.module.css";
 
 const Counter = () => {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+
   return (
     <div>
       <h1 className={styles.counter}>Counter</h1>
-      <button className={styles.button}>-</button>
-      <span className={styles.count}>0</span>
-      <button className={styles.button}>+</button>
+      <div className={styles.container}>
+        <button className={styles.button} onClick={decrement}>
+          -
+        </button>
+        <span className={styles.count}>{count}</span>
+        <button className={styles.button} onClick={increment}>
+          +
+        </button>
+      </div>
     </div>
   );
 };
